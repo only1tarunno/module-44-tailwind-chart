0760d8b06106a9eeadabf358dbe83a0b0c5cb0c6
only1tarunno <tarunno281@gmail.com>
2023-09-17 18:22:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+// import PropTypes from 'prop-types';
+// MyComponent.propTypes = {
+
+//   optionalArray: PropTypes.array,
+// }
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
